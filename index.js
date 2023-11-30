f702a8809a2383ad3804f41bf7440ae8c8f45c8a
Franco Amori <francoamori47@gmail.com>
2023-11-30 05:50:40 -0300

    Create function to play a single round

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function getComputerChoice() {
     
     return choices[choice]
 }
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) return "Tie"
+
+    if (playerChoice === "rock" && computerChoice === "paper") return "You lose! Paper beats rock"
+    if (playerChoice === "rock" && computerChoice === "scissors") return "You win! Rock beats scissors"
+
+    if (playerChoice === "paper" && computerChoice === "rock") return "You win! Paper beats rock"
+    if (playerChoice === "paper" && computerChoice === "scissors") return "You lose! Scissors beats paper"
+
+    if (playerChoice === "scissors" && computerChoice === "paper") return "You win! Scissors beats paper"
+    if (playerChoice === "scissors" && computerChoice === "rock") return "You lose! Rock beats scissors"
+}
+
